a7ce8c61a8d1d377a610b3f0887ce709ec05c370
aliarnoos <alimarnoos@gmail.com>
2024-09-02 16:06:51 +0300

    feat: add Activity Log resource

---
+++ app/Filament/Resources/ActivityLogResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ActivityLogResource\Pages;
+use App\Filament\Resources\ActivityLogResource\RelationManagers;
+use App\Models\ActivityLog;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Spatie\Activitylog\Models\Activity;
+use Filament\Tables\Columns\TextColumn;
+
+class ActivityLogResource extends Resource
+{
+    protected static ?string $model = Activity::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('description')->label('Description')->sortable()->searchable(),
+                TextColumn::make('causer.name')->label('User')->sortable()->searchable(),
+                TextColumn::make('subject_type')->label('Subject Type')->sortable()->searchable(),
+                TextColumn::make('properties.old')->label('Old Values')->wrap(),
+                TextColumn::make('properties.new')->label('New Values')->wrap(),
+                TextColumn::make('created_at')->label('Created At')->dateTime()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+            ])
+            ->bulkActions([
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListActivityLogs::route('/'),
+        ];
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+}
