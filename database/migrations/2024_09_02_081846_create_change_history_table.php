fd3fc90eeda14ec6e1020a8339430f2e36da97cc
aliarnoos <alimarnoos@gmail.com>
2024-09-02 13:07:50 +0300

    fix: fix table name typo

--- database/migrations/2024_09_02_081846_create_change_history_table.php
+++ database/migrations/2024_09_02_081846_create_change_history_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('change_history', function (Blueprint $table) {
+        Schema::create('change_histories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('link_id')->constrained()->onDelete('cascade');
             $table->text('old_content')->nullable(); 
