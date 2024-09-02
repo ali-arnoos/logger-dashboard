154cc1183ccdef8998bf618e153a804ede007201
aliarnoos <alimarnoos@gmail.com>
2024-09-02 16:08:11 +0300

    feat: add log when history is created

--- app/Models/ChangeHistory.php
+++ app/Models/ChangeHistory.php
@@ -11,6 +11,18 @@ class ChangeHistory extends Model
 
     protected $fillable = ['link_id', 'old_content', 'new_content', 'change_location', 'user_id', 'name' ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($link) {
+            activity()
+                ->performedOn($link)
+                ->causedBy(auth()->user())
+                ->log('History created');
+        });
+    }
+
     public function link()
     {
         return $this->belongsTo(Link::class);
